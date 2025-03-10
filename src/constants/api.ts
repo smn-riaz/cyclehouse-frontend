e213a4789a2a9bc1861696f4c149ae4a68d50afa
shahman-riaz <shahmanriaz07@gmail.com>
2025-03-11 03:05:34 +0600

    done

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-// export const rootBackendApi = "https://bicycle-store-assignment4-backend.vercel.app/api"
-export const rootBackendApi = "http://localhost:5002/api"
+export const rootBackendApi = "https://bicycle-store-assignment4-backend.vercel.app/api"
+// export const rootBackendApi = "http://localhost:5002/api"
 
 
